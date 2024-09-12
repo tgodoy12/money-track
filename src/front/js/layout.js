b83f4981ae96ff65905ace08350146924ab886bc
Trilce Godoy <112527761+tgodoy12@users.noreply.github.com>
2024-09-12 22:59:58 +0000

    landing page finding its own styles

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,11 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
+
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
-
+import Home from "./pages/home.js";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
@@ -23,14 +23,14 @@ const Layout = () => {
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
+                    {/* <Navbar /> */}
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={ <Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
+                    {/* <Footer /> */}
                 </ScrollToTop>
             </BrowserRouter>
         </div>

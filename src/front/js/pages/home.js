08b7204bca3526dd93bfd634a3f6dab20c321b5c
Trilce Godoy <112527761+tgodoy12@users.noreply.github.com>
2024-09-13 00:54:49 +0000

    landing page con colores definidos casi pronta. Faltan animations

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,31 +1,30 @@
 import React, { useContext} from "react";
-'use client'
-
+import icon from "../../img/icon.png"
+import "../../styles/home.css"
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+	{ name: 'Features', href: '#' },
+	{ name: 'Pricing', href: '#' },
+	{ name: 'Contact', href: '#' },
 ]
 
 const Home = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <div className="bg-white">
+    <div className="landing-page min-h-screen">
       <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="#" className="-m-1.5 p-1.5 transition-all duration-300 ease-in-out hover:p-3" style={{ width: '80px', height: '60px' }}>
               <span className="sr-only">Money-Tracker</span>
               <img
                 alt="Money-Tracker Logo"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
+                src={icon}
+                className="h-10 w-auto"
               />
             </a>
           </div>
@@ -39,15 +38,15 @@ const Home = () => {
               <Bars3Icon aria-hidden="true" className="h-6 w-6" />
             </button>
           </div>
-          <div className="hidden lg:flex lg:gap-x-12">
+          <div className="navigation-list hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+              <a key={item.name} href={item.href} className="text-sm font-medium leading-6 duration-200 ease-in-out hover:scale-105">
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="#" className="login text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -110,26 +109,26 @@ const Home = () => {
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
             }}
-            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#CBF7ED] to-[#406E8E] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            <h1 className="landing-title text-4xl font-bold tracking-tight sm:text-6xl">
               Take Control of Your Finances
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="landing-text mt-6 text-lg leading-8 text-gray-600">
               Keep track of your expenses, set budgets, and manage your accounts all in one place. 
               Money-Tracker helps you stay on top of your financial goals effortlessly.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="button-get-started rounded-md  px-3.5 py-2.5 text-sm font-semibold shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Get Started
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a href="#" className="button-learn-more text-sm font-semibold leading-6 ">
                 Learn More <span aria-hidden="true">→</span>
               </a>
             </div>
